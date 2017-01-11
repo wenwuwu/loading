a82350737d2e4b6fe3bcbbebcf3d52d3f4565979
Wu Wenwu <wen.v.wu@gmail.com>
2017-01-11 20:49:53 +0800

    .

---
+++ server.js
@@ -0,0 +1,39 @@
+
+var express = require('express')
+var compression = require('compression')
+var path = require('path')
+var fs = require('fs')
+var readline = require('readline')
+var port = process.env.PORT || 3000
+var app = express()
+var nodeEnv = process.env.NODE_ENV || 'development'
+var isPro = nodeEnv === 'production'
+
+app.use(compression({filter: shouldCompress}))
+
+function shouldCompress (req, res) {
+    if (   req.headers['x-no-compression']
+        || (/\.(png|jpg|gif|pdf)$/.test(req.path)) ) {
+        return false;
+    }
+    return compression.filter(req, res);
+};
+
+function serveIndexPage (res) {
+    response.sendFile(path.resolve(__dirname, '/', 'index.html'));    // Make sure don't cache it.
+};
+
+app.use(express.static(__dirname + '/component'));
+
+app.get('*', function (req, res) {
+    serveIndexPage(res);
+});
+
+app.use(function(err, req, res, next) {
+    console.error(err.stack);
+    res.status(500);
+    res.render('error', { error: err });
+});
+console.log(`Server is running in ${nodeEnv} mode on port ${port}`);
+
+app.listen(port)
